834e74992cb8f74117a6b7d504fefb55adfcb635
Patricia2997 <patricia.lema@campusdual.com>
2023-11-08 15:49:03 +0100

    Cat

--- exercises/Cat.java
+++ exercises/Cat.java
@@ -2,46 +2,46 @@ package com.campusdual.exercises;
 
 public class Cat {
     /*
-     Crea una clase llamada Cat
-     La clase debe tener las siguientes propiedades sin inicializar:
-     - nombre (name)
-     - raza (breed)
-     - sexo (sex)
-     - edad en meses (age) (estática)
-     - tamaño del pelo (hair)
-     - color del pelo (colour) (estática)
-     - un booleano que indique si está castrado/a o no (castrated)I
+Crea una clase llamada Cat
+La clase debe tener las siguientes propiedades sin inicializar:
+- nombre (name)
+- raza (breed)
+- sexo (sex)
+- edad en meses (age) (estática)
+- tamaño del pelo (hair)
+- color del pelo (colour) (estática)
+- un booleano que indique si está castrado/a o no (castrated)I
 
-     La clase debe tener las siguientes propiedades con los valores inicializados
-     - el color de los ojos (eyeColour): "green"
+La clase debe tener las siguientes propiedades con los valores inicializados
+- el color de los ojos (eyeColour): "green"
 
-     Crea un constructor que incluya todos los atributos no inicializados
+Crea un constructor que incluya todos los atributos no inicializados
 
-     Crea un método estático que cambie el color del pelo y otro no estático que cambie el color de los ojos
+Crea un método estático que cambie el color del pelo y otro no estático que cambie el color de los ojos
 
-     Crea un método estático que cambie la edad del gato y otro no estático que permita recuperar ese valor cuando se invoque
+Crea un método estático que cambie la edad del gato y otro no estático que permita recuperar ese valor cuando se invoque
 
-     Crea un método no estático que castre a los gatos y otro no estático, llamado isCastrated, que devuelva su estado
+Crea un método no estático que castre a los gatos y otro no estático, llamado isCastrated, que devuelva su estado
 
-     Crea un método no estático llamado catDetails() que muestre todas las características de un gato (sé original en la presentación de los datos, dale un poco de arte)
+Crea un método no estático llamado catDetails() que muestre todas las características de un gato (sé original en la presentación de los datos, dale un poco de arte)
 
-     Crea un main()
+Crea un main()
 
-     Crea un gato de la raza siamesa, otro de raza persa, y otro de raza esfinge
+Crea un gato de la raza siamesa, otro de raza persa, y otro de raza esfinge
 
-     Castra a los dos primeros gatos cambiando su booleano a true. Deja el otro gato sin castrar (false)
+Castra a los dos primeros gatos cambiando su booleano a true. Deja el otro gato sin castrar (false)
 
-     Cámbiale el color del pelo a los dos primeros gatos
+Cámbiale el color del pelo a los dos primeros gatos
 
-     Cámbiale el color de los ojos al último gato
+Cámbiale el color de los ojos al último gato
 
-     Cambia la edad del último gato. Intenta ponerle un valor negativo (su setter debe hacer la comprobación)
+Cambia la edad del último gato. Intenta ponerle un valor negativo (su setter debe hacer la comprobación)
 
-     Muestra los detalles de cada gato (fijaros qué dato muestra en la edad y el color del pelo)
+Muestra los detalles de cada gato (fijaros qué dato muestra en la edad y el color del pelo)
 
-     Vuelve a hacer el ejercicio en una nueva clase CatEncapsulated, aplicando la encapsulación (atributos private, ninguno estático, getters y setters)
-      Verás cómo afecta a los métodos estáticos
-    */
+Vuelve a hacer el ejercicio en una nueva clase CatEncapsulated, aplicando la encapsulación (atributos private, ninguno estático, getters y setters)
+  Verás cómo afecta a los métodos estáticos
+*/
     public String name;
     public String breed;
     public static int age; // hacer cambios: STATIC
@@ -60,24 +60,9 @@ public class Cat {
         this.castrado = castrado;
         this.sex = sex;
     }
+
     public boolean isCastrado() {
         return castrado;
-    }
-
-    public void setCastrado(boolean castrado) {
-        this.castrado = castrado;
-    }
-
-    public String getEyeColour() {
-        return eyeColour;
-    }
-
-    public void setEyeColour(String eyeColour) {
-        this.eyeColour = eyeColour;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public static void changeColour(String newColour) {
@@ -88,8 +73,7 @@ public class Cat {
         System.out.println("El nuevo color de ojos es azul ");
         this.eyeColour = eyeColour = "Azul";
     }
-
-
+        
     public static void changeAge(int newAge) {
         if (newAge > 0) {
             Cat.age = newAge;
@@ -140,5 +124,6 @@ public class Cat {
         System.out.println("  ");
     } // CAAAT
 }
+
 
 
